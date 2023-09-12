3c4efc79455ac82bc93ad8a8509b16c641ee0446
Kitan <65513837+Ktn-lwl@users.noreply.github.com>
2023-09-12 21:56:13 +0100

    Update Grid.py

--- Grid.py
+++ Grid.py
@@ -1,14 +1,4 @@
-"""
-DISCLAIMER: 
-This is my first full project in Python. I'm trying to write code that takes 
-a set of words as input and builds a word search puzzle from them. I only 
-have a vague idea of what I'm doing, so I'm just making it up as I go along.
-My logic and time complexity may be shoddy in some areas, but I will refactor
-sections of my code as I go along. I've decided to make a solver first, because
-that seems easier than a puzzle generator.
-
-r and c refer to rows and columns respectively throughout code
-"""
+#r and c refer to rows and columns respectively throughout code
 
 class Grid:
     def __init__(self, dimensions): 
